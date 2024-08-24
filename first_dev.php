7f1cf9d4f5346283958f37e9e7d353c828b19ba9
NetworkCrab <network123crab@gmail.com>
2024-08-24 10:58:25 +0530

    This is the first web_page.....

---
+++ first_dev.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>First_Development</title>
+</head>
+<body>
+    <?php
+        echo "Hello World !";
+        echo "Result".(5+98);
+    ?>
+</body>
+</html>
