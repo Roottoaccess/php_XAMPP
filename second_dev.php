a2b1cae26b213cde946b7849d6b8d6a400c1a4aa
NetworkCrab <network123crab@gmail.com>
2024-08-24 11:50:21 +0530

    Learning how we can use PHP with bootstrap to make our websites more attractive and practical.....

---
+++ second_dev.php
@@ -0,0 +1,53 @@
+<!-- This is the bootstrap templet -->
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <!-- This is the CSS part -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#"><?php echo "Company Name"; ?></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#"><?php echo "Departments"; ?></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"><?php echo "Salaries"; ?></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"><?php echo "Systems"; ?></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="alert alert-warning alert-dismissible fade show" role="alert">
+        <strong>High Alert!</strong> This is a PHP web_server tutorial.
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+    <h1>
+        <?php
+        // This is simple Hello World in PHP, more workings are coming up.....
+        echo "Hello World !";
+        ?>
+    </h1>
+    <hr>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
