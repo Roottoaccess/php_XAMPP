bbf357de0140cf94ea8e396f2b2fffcba4cf6ac3
NetworkCrab <network123crab@gmail.com>
2024-09-04 11:54:44 +0530

    This is the date_function in PHP scripting....

---
+++ Date_function.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Date_Function</title>
+</head>
+<body>
+    <?php
+        echo "Welcome to the world of dates....<br>";
+
+        $d = date("dS F Y, g:i A");
+        echo "Todays date is $d <br>";
+    ?>
+</body>
+</html>
