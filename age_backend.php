004744a47837b8e3caa3ebe6754687ba871a0b3e
NetworkCrab <network123crab@gmail.com>
2024-09-05 15:09:10 +0530

    This is the voting eligibility checking through html and PHP using in backend....

---
+++ age_backend.php
@@ -0,0 +1,83 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="style.css">
+
+    <title>Voting Eligibility Checking Machine</title>
+</head>
+
+<body>
+    <!-- Optional JavaScript; choose one of the two! -->
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script>
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
+        integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
+        integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF"
+        crossorigin="anonymous"></script>
+
+    <!-- Creating the navbar -->
+    <!-- As a link -->
+    <!-- <nav class="navbar navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Navbar</a>
+        </div>
+    </nav> -->
+
+    <!-- As a heading -->
+    <nav class="navbar navbar-dark bg-dark">
+        <div class="container-fluid">
+            <span class="navbar-brand mb-0 h1">Voting Eligibily Checking System</span>
+        </div>
+    </nav>
+    <br><br>
+    <div class="container">
+        <form action="/WORK_STATION/age_backend.php" method="POST">
+            <div class="mb-3">
+              <label for="age" class="form-label">Enter Your Current Age: </label>
+              <input type="number" class="form-control" id="age" name="age" aria-describedby="emailHelp">
+            </div>
+            <button type="submit" class="btn btn-primary" value="check">Submit</button>
+          </form>
+          <br>
+          <?php
+
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+    $age = $_POST["age"];
+
+    if($age >= 18){
+        echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
+  <strong>Eligible to vote</strong>
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>';
+    } else if($age < 0){
+        echo '<div class="alert alert-warning alert-dismissible fade show" role="alert">
+  <strong>Wrong Input</strong>
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>';
+    } else {
+        echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">
+  <strong>Not Eligible to vote</strong>
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>';
+    }
+}
+
+?>
+    </div>
+</body>
+
+</html>
